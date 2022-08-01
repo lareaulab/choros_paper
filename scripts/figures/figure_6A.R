f5bc5a77e4f8b9730939e85c7f2d6e6d86b79066
Amanda Mok <a.mok@berkeley.edu>
2022-08-01 08:02:17 -0700

    fix figure 6A

--- scripts/figures/figure_6A.R
+++ scripts/figures/figure_6A.R
@@ -21,13 +21,13 @@ for(expt in expts) {
 # generate plot -----------------------------------------------------------
 
 coef_WT <- subset(CHXTIG_WT_coef, group=="A")
-colnames(coef_WT)[match(c("estimate", "std_error"), colnames(coef_WT))] <- c("est_WT", "stderr_WT")
 coef_3AT <- subset(CHXTIG_3AT_coef, group=="A")
-colnames(coef_3AT)[match(c("estimate", "std_error"), colnames(coef_3AT))] <- c("est_3AT", "stderr_3AT")
-coef_3AT <- coef_3AT[match(coef_3WT$term, coef_3AT$term),]
-
-coef_A <- cbind(coef_WT[, c("term", "est_WT", "stderr_WT")],
-                coef_3AT[, c("est_3AT", "stderr_3AT")])
+codons <- coef_A$term
+coef_A <- data.frame(codons=codons,
+                     est_WT=coef_WT$estimate[match(codons, coef_WT$term)],
+                     stderr_WT=coef_WT$std_error[match(codons, coef_WT$term)],
+                     est_3AT=coef_3AT$estimate[match(codons, coef_3AT$term)],
+                     stderr_3AT=coef_3AT$std_error[match(codons, coef_3AT$term)])
 
 figure_6A <- ggplot(coef_A,
                     aes(x=est_WT, y=est_3AT,
@@ -40,7 +40,7 @@ figure_6A <- ggplot(coef_A,
   geom_errorbarh(color="grey25", alpha=0.5) +
   theme_classic(base_size=6) + geom_abline(slope=1, intercept=0, color="blue") +
   geom_text(data=subset(coef_A, abs(est_WT)>2 | abs(est_3AT)>2), color="grey25",
-            aes(label=term), position=position_nudge(x=0.2, y=0.2), size=2) +
+            aes(label=codons), position=position_nudge(x=0.2, y=0.2), size=2) +
   xlab(expression("WT "*beta^A)) + ylab(expression("3AT "*beta^A))
 
 ggsave(filename=file.path(figures_dir, "figure_6A.pdf"),
